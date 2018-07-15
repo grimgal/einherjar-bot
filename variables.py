38a74b5b4a1c34babe8b46e7e2806b11d35afbba
Dissi <scriptingdays@gmail.com>
2018-07-15 10:07:34 -0700

    csv

--- variables.py
+++ variables.py
@@ -33,7 +33,7 @@ c = pd.read_csv(url,encoding='utf-8')
 count = 0
 Skill = collections.namedtuple('Skill','name jp mp description owner learn')
 while count < 172:
-    skill = Skill(name=c['Name'][count],jp=c['JP Name'][count],mp=str(c['Cost'][count]),description=str(c['Description'][count]),owner=str(c['Owner'][count]),learn=str(c['Transfer'][count]))
+    skill = Skill(name=c['Name'][count],jp=c['JP Name'][count],mp=str(c['Cost'][count]),description=str(c['Description'][count]),owner=c['Learners'][count],learn=c['Transfer'][count])
     skills[skill.name.lower()] = "```md\n#" + skill.name + " | " + skill.jp + " | " + skill.mp + " | " + skill.description + "\nDemons with skill: " + skill.owner + "\nDemons to transfer skill from: " + skill.learn + "```"
     count += 1
 
