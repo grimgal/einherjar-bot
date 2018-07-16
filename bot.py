5cb6a9d11a085aa2994e07a70c205475adaa2dd0
Dissi <scriptingdays@gmail.com>
2018-07-16 05:14:12 -0700

    Better formatting for skills display

--- bot.py
+++ bot.py
@@ -24,6 +24,73 @@ def get_skill_list(skill, arch = ''):
         return [arch, skills[0], skills[2], skills[3]]
     return [skills[0],skills[2],skills[3]]
 
+def largest_character_count (s):
+    l = 0
+    for item in s:
+        if len(item) > l:
+            l = len(item)
+    return l
+
+# Returns skill table
+def get_skills(d, t):
+    r = ""
+    if t == 1:
+        count = 1
+        while count < 4:
+            table = Texttable()
+            table.set_deco(Texttable.VLINES)
+            table.set_cols_align(["l", "c", "c", "l"])
+            table.set_cols_width([7, largest_character_count([d.s1.split('|')[0], d.s2.split('|')[0], d.s3.split('|')[0]]), 7,150])
+
+            if count == 1:
+                table.add_row(get_skill_list(d.s1, "Skill 1"))
+            elif count == 2:
+                table.add_row(get_skill_list(d.s2, "Skill 2"))
+            elif count == 3:
+                table.add_row(get_skill_list(d.s3, "Skill 3"))
+            r = r + table.draw().strip() + "\n"
+            count += 1
+    elif t == 2:
+        count = 0
+        while count < 5:
+            table = Texttable()
+            table.set_deco(Texttable.VLINES)
+            table.set_cols_align(["l", "c", "c", "l"])
+            table.set_cols_width([19, largest_character_count([d.ca.split('|')[0], d.cr.split('|')[0], d.cy.split('|')[0], d.cp.split('|')[0], d.ct.split('|')[0]]), 7,150])
+
+            if count == 0:
+                table.add_row(get_skill_list(d.ca, "Common (Clear)"))
+            elif count == 1:
+                table.add_row(get_skill_list(d.cr, "Aragami (Red)"))
+            elif count == 2:
+                table.add_row(get_skill_list(d.cy, "Protector (Yellow)"))
+            elif count == 3:
+                table.add_row(get_skill_list(d.cp, "Psychic (Purple)"))
+            elif count == 4:
+                table.add_row(get_skill_list(d.ct, "Elementalist (Teal)"))
+            r = r + table.draw().strip() + "\n"
+            count += 1
+    elif t == 3:
+        count = 0
+        while count < 4:
+            table = Texttable()
+            table.set_deco(Texttable.VLINES)
+            table.set_cols_align(["l", "c", "c", "l"])
+            table.set_cols_width([19, largest_character_count([d.gr.split('|')[0], d.gy.split('|')[0], d.gp.split('|')[0], d.gt.split('|')[0]]), 7,150])
+
+            if count == 0:
+                table.add_row(get_skill_list(d.gr, "Aragami (Red)"))
+            elif count == 1:
+                table.add_row(get_skill_list(d.gy, "Protector (Yellow)"))
+            elif count == 2:
+                table.add_row(get_skill_list(d.gp, "Psychic (Purple)"))
+            elif count == 3:
+                table.add_row(get_skill_list(d.gt, "Elementalist (Teal)"))
+            r = r + table.draw().strip() + "\n"
+            count += 1
+
+    return r
+
 # Read and store raw demon data from csv
 demons = {}
 demon_names = []
@@ -52,37 +119,32 @@ while count < 172:
     [demon.hp,demon.str,demon.mag,demon.vit,demon.agi,demon.luk]])
 
     resistsTable = Texttable()
+    #resistsTable.set_deco()
     resistsTable.set_cols_align(["c", "c", "c", "c", "c", "c", "c"])
     resistsTable.add_rows([["Physical","Fire","Ice","Electricity","Force","Light","Dark"],
     [demon.phys,demon.fire,demon.ice,demon.elec,demon.force,demon.light,demon.dark]])
 
-    #skillsTable = Texttable()
-    #skillsTable.set_cols_align(["c", "c", "c"])
-    #skillsTable.add_rows([["Skill Name","MP Cost","Description"],get_skill_list(demon.s2),get_skill_list(demon.s1),get_skill_list(demon.s3)])
-    #archtypeTable = Texttable()
-    #archtypeTable.set_cols_align(["c", "c", "c", "c"])
-    #archtypeTable.add_rows([["Archetype","Skill Name","MP Cost","Description"],get_skill_list(demon.ca,"Common"),get_skill_list(demon.cr,"Aragami"),get_skill_list(demon.cy,"Protector"),get_skill_list(demon.cp,"Psychic"),get_skill_list(demon.ct,"Elementalist")])
-    #gachaTable = Texttable()
-    #gachaTable.set_cols_align(["c", "c", "c", "c"])
-    #gachaTable.add_rows([["Archetype","Skill Name","MP Cost","Description"],get_skill_list(demon.gr,"Aragami"),get_skill_list(demon.gy,"Protector"),get_skill_list(demon.gp,"Psychic"),get_skill_list(demon.gt,"Elementalist")])
+    innateSkills = get_skills(demon, 1)
+    archSkills = get_skills(demon, 2)
+    gachaSkills = get_skills(demon, 3)
 
     #demonText = "```md\n#" + demon.name + "\n" + infoTable.draw()
     #demonText = demonText + "\n\n#6☆ Max Level Stats\n" + statsTable.draw()
+    #demonText = demonText + "\n\n#Elemental Resistances\nPhysical: " + demon.phys + " | Fire: " + demon.fire + " | Ice: " + demon.ice + " | Electricity: " + demon.elec + " | Force: " + demon.force + " | Light: " + demon.light + " | Dark: " + demon.dark
     demonText = "```md\n#" + demon.name + "\nRace: " + demon.race + " | Grade: " + demon.grade + " | Rarity: " + demon.rarity
     demonText = demonText + "\n\n#6☆ Max Level Stats\nHP - " + demon.hp + " | Strength - " + demon.str + " | Magic - " + demon.mag + " | Vitality - " + demon.vit + " | Agility - " + demon.agi + " | Luck - " + demon.luk
     demonText = demonText + "\n\n#Elemental Resistances\n" + resistsTable.draw()
-    demonText = demonText + "\n\n#Innate Skills:"\
-    + "\nSkill 1 | " + demon.s1.replace('|',' | ') + "\nSkill 2 | " + demon.s2.replace('|',' | ') + "\nSkill 3 | " + demon.s3.replace('|',' | ')
-    demonText = demonText + "\n\n#Archetype Skills:\nCommon (Clear)      | " + demon.ca.replace('|',' | ')\
-    + "\nAragami (Red)       | " + demon.cr.replace('|',' | ') + "\nProtector (Yellow)  | " + demon.cy.replace('|',' | ')\
-    + "\nPsychic (Purple)    | " + demon.cp.replace('|',' | ') + "\nElementalist (Teal) | " + demon.ct.replace('|',' | ')
-
-    demonText = demonText + "\n\n#Gacha Skills:"\
-    + "\nAragami (Red)       | " + demon.gr.replace('|',' | ') + "\nProtector (Yellow)  | " + demon.gy.replace('|',' | ')\
-    + "\nPsychic (Purple)    | " + demon.gp.replace('|',' | ') + "\nElementalist (Teal) | " + demon.gt.replace('|',' | ')
-
-    #demonText = demonText + "\n\n#6☆ Max Level Stats\nHP - " + demon.hp + " | Strength - " + demon.str + " | Magic - " + demon.mag + " | Vitality - " + demon.vit + " | Agility - " + demon.agi + " | Luck - " + demon.luk
-    #demonText = demonText + "\n\n#Elemental Resistances\nPhysical: " + demon.phys + " | Fire: " + demon.fire + " | Ice: " + demon.ice + " | Electricity: " + demon.elec + " | Force: " + demon.force + " | Light: " + demon.light + " | Dark: " + demon.dark
+    demonText = demonText + "\n\n#Innate Skills\n" + innateSkills
+    demonText = demonText + "\n#Archetype Skills\n" + archSkills
+    demonText = demonText + "\n#Gacha Skills\n" + gachaSkills
+    #demonText = demonText + "\n\n#Innate Skills:"\
+    #+ "\nSkill 1 | " + demon.s1.replace('|',' | ') + "\nSkill 2 | " + demon.s2.replace('|',' | ') + "\nSkill 3 | " + demon.s3.replace('|',' | ')
+    #demonText = demonText + "\n\n#Archetype Skills:\nCommon (Clear)      | " + demon.ca.replace('|',' | ')\
+    #+ "\nAragami (Red)       | " + demon.cr.replace('|',' | ') + "\nProtector (Yellow)  | " + demon.cy.replace('|',' | ')\
+    #+ "\nPsychic (Purple)    | " + demon.cp.replace('|',' | ') + "\nElementalist (Teal) | " + demon.ct.replace('|',' | ')
+    #demonText = demonText + "\n\n#Gacha Skills:"\
+    #+ "\nAragami (Red)       | " + demon.gr.replace('|',' | ') + "\nProtector (Yellow)  | " + demon.gy.replace('|',' | ')\
+    #+ "\nPsychic (Purple)    | " + demon.gp.replace('|',' | ') + "\nElementalist (Teal) | " + demon.gt.replace('|',' | ')
     demons[demon.name.lower().replace("'",'')] = demonText + "```"
     demon_names.append(demon.name)
     count += 1
